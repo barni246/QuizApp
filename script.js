0d474a46cfef418004e3bb96ac78646734136760
Barnabas Gonda <118932791+barni246@users.noreply.github.com>
2022-11-27 20:23:09 +0100

    JSON

---
+++ script.js
@@ -0,0 +1,59 @@
+let questions = [
+
+    {
+        "question": "Wer die ersten Zündhölzer erfunden?",
+        "answer_1": "",
+        "answer_2": "",
+        "answer_3": "Jean-Louis Chancel",
+        "answer_4": "",
+        "right_answer": 3
+    },
+    {
+        "question": "Wer hat die Impfung erfunden?",
+        "answer_1": "",
+        "answer_2": "",
+        "answer_3": "",
+        "answer_4": "Edward Jenner",
+        "right_answer": 4
+    },
+    {
+        "question": "Wieviel  gramm wiegt ein Würfel Zucker?",
+        "answer_1": 3,
+        "answer_2": 5,
+        "answer_3": 7,
+        "answer_4": 10,
+        "right_answer": 1
+    },
+    {
+        "question": "Wer hat John Lennon erschossen?",
+        "answer_1": "",
+        "answer_2": "",
+        "answer_3": "Mark David Chapman",
+        "answer_4": "",
+        "right_answer": 3
+    },
+    {
+        "question": "Wer hat HTML erfunden?",
+        "answer_1": "Robbie Williams",
+        "answer_2": "Lady Gaga",
+        "answer_3": "Tim Berners-Lee",
+        "answer_4": "Justin Bieber",
+        "right_answer": 3
+    },
+    {
+        "question": "Wer hat HTML erfunden?",
+        "answer_1": "Robbie Williams",
+        "answer_2": "Lady Gaga",
+        "answer_3": "Tim Berners-Lee",
+        "answer_4": "Justin Bieber",
+        "right_answer": 3
+    },
+    {
+        "question": "Wer hat HTML erfunden?",
+        "answer_1": "Robbie Williams",
+        "answer_2": "Lady Gaga",
+        "answer_3": "Tim Berners-Lee",
+        "answer_4": "Justin Bieber",
+        "right_answer": 3
+    }
+];
